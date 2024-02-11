a8855dfc374006dd5a7cdc98ed69be69dd8c1115
Yviee <c.muckenhumer@gmx.net>
2024-02-11 18:12:23 +0100

    updated TopBar to use a more default implementation of the TopAppBar

--- app/src/main/java/com/example/kanjimemory/sharedComposables/TopBar.kt
+++ app/src/main/java/com/example/kanjimemory/sharedComposables/TopBar.kt
@@ -1,45 +1,38 @@
 package com.example.kanjimemory.sharedComposables
 
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.interaction.MutableInteractionSource
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.material.TopAppBar
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
-import androidx.compose.material.ripple.rememberRipple
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.remember
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import com.example.kanjimemory.ui.theme.Purple200
 
 @Composable
 fun TopBar(navController: NavController) {
-    TopAppBar(elevation = 3.dp, backgroundColor = Purple200) {
-        Row {
-            Icon(
-                imageVector = Icons.AutoMirrored.Filled.ArrowBack,
-                contentDescription = "Arrow back",
-                modifier = Modifier
-                    .clip(CircleShape)
-                    .clickable(
-                        onClick = { navController.popBackStack() },
-                        indication = rememberRipple(),
-                        interactionSource = remember { MutableInteractionSource() })
-            )
-            Spacer(modifier = Modifier.width(20.dp))
+    TopAppBar(
+        title = {
             Text(
                 text = "Back to Main Menu",
                 style = MaterialTheme.typography.subtitle1
             )
+        },
+        elevation = 3.dp,
+        backgroundColor = Purple200,
+        navigationIcon = {
+            IconButton(
+                onClick = { navController.popBackStack() },
+                content = {
+                    Icon(
+                        imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+                        contentDescription = "BackArrow"
+                    )
+                }
+            )
         }
-    }
+    )
 }
